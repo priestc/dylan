6bbc2bec8177ccc4b1ffd870c5cacdff00011211
Chris Priest <cp368202@ohiou.edu>
2013-01-01 02:49:17 -0500

    removed id from album json

--- views.py
+++ views.py
@@ -2,7 +2,14 @@ import json
 from giotto.views import BasicView
 
 def album2json(result):
-    songs = [x.todict() for x in result['album'].songs]
+    """
+    Convert the album instance into a json object. Include the list of
+    songs as well. Songs do not include the ID.
+    """
+    new_songs = []
+    for song in [x.todict() for x in result['album'].songs]:
+        del song['id']
+        new_songs.append(song)
     album = result['album'].todict()
-    album['songs'] = songs
+    album['songs'] = new_songs
     return BasicView().generic_json(album, None)
