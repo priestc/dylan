4d5381962bcc403351ebcbb86b65ab33b9a77dad
Chris Priest <cp368202@ohiou.edu>
2013-08-05 13:17:10 -0700

    renamed a bunch of stuff, fixed static

--- manifest.py
+++ manifest.py
@@ -1,5 +1,5 @@
 from giotto import get_config
-from giotto.programs import GiottoProgram, ProgramManifest
+from giotto.programs import Program, Manifest
 from giotto.programs.management import management_manifest
 from giotto.views import BasicView, jinja_template
 from giotto.contrib.static.programs import StaticServe, SingleStaticServe
@@ -10,22 +10,22 @@ from mocks import mock_album, mock_songs, mock_venues, mock_venue, mock_song
 
 from views import album2json
 
-manifest = ProgramManifest({
-    '': GiottoProgram(
+manifest = Manifest({
+    '': Program(
         model=[Album.most_recent],
         cache=3600,
         view=BasicView(
             html=jinja_template('home.html'),
         ),
     ),
-    'albums': GiottoProgram(
+    'albums': Program(
         model=[Album.all, {'albums': [mock_album]}],
         cache=3600,
         view=BasicView(
             html=jinja_template('all_albums.html'),
         ),
     ),
-    'album': GiottoProgram(
+    'album': Program(
         model=[Album.get, {'album': mock_album}],
         cache=3600,
         view=BasicView(
@@ -33,65 +33,65 @@ manifest = ProgramManifest({
             json=album2json,
         ),
     ),
-    'song': GiottoProgram(
+    'song': Program(
         model=[Song.profile, mock_song],
         cache=3600,
         view=BasicView(
             html=jinja_template('song.html'),
         ),
     ),
-    'songs': GiottoProgram(
+    'songs': Program(
         model=[Song.all_songs, mock_songs],
         cache=3600,
         view=BasicView(
             html=jinja_template('all_songs.html'),
         ),
     ),
-    'venue': GiottoProgram(
+    'venue': Program(
         model=[Album.by_venue, mock_venue],
         cache=3600,
         view=BasicView(
             html=jinja_template('venue.html'),
         )
     ),
-    'venues': GiottoProgram(
+    'venues': Program(
         model=[Album.all_venues, mock_venues],
         cache=3600,
         view=BasicView(
             html=jinja_template('all_venues.html'),
         )
     ),
-    'city': GiottoProgram(
+    'city': Program(
         model=[Album.by_city],
         cache=3600,
         view=BasicView(
             html=jinja_template('city.html'),
         )
     ),
-    'cities': GiottoProgram(
+    'cities': Program(
         model=[Album.all_cities],
         cache=3600,
         view=BasicView(
             html=jinja_template('all_cities.html'),
         )
     ),
-    'bucket_contents': GiottoProgram(
+    'bucket_contents': Program(
         model=[get_bucket_contents, [{"filename": '1.mp3', "duration": "2:12"}, {"filename": '2.mp3', "duration": "7:50"}]],
         view=BasicView(),
     ),
-    'newest': GiottoProgram(
+    'newest': Program(
         model=[Album.newest],
         view=BasicView(
             html=jinja_template('newest.html'),
         ),
     ),
     'new': [
-        GiottoProgram(
+        Program(
             view=BasicView(
                 html=jinja_template('album_form.html'),
             ),
         ),
-        GiottoProgram(
+        Program(
             controllers=['http-post'],
             model=[Album.create],
             view=BasicView(
@@ -99,22 +99,22 @@ manifest = ProgramManifest({
             ),
         ),
     ],
-    'load_all_data': GiottoProgram(
+    'load_all_data': Program(
         controllers=['cmd'],
         model=[add_all],
         view=BasicView(),
     ),
-    'add_album': GiottoProgram(
+    'add_album': Program(
         controllers=['cmd'],
         model=[add_album],
         view=BasicView(),
     ),
-    'reload_album': GiottoProgram(
+    'reload_album': Program(
         controllers=['cmd'],
         model=[reload_album],
         view=BasicView,
     ),
     'favicon': SingleStaticServe(get_config('project_path') + '/static/favicon.ico'),
-    'static': StaticServe(get_config('project_path') + '/static/'),
+    'static': StaticServe('/static/'),
     'mgt': management_manifest,
 })
